2655e2feff58eab0e134d5beee8bb3311f1b36e9
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2026-06-19 17:32:22 +0000

    commit

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const CastleLineColor = Object.freeze({
 const CommandCursorColor = "#0056ff";
 
 // Draw background and a sample green area at logical coordinates
-ctx.fillStyle = Color.RED_BROWN;
+ctx.fillStyle = Color.BLACK;
 ctx.fillRect(0, 0, INTERNAL_WIDTH, INTERNAL_HEIGHT);
 ctx.fillStyle = Color.GREEN;
 ctx.fillRect(0, 0, 256, 224);
